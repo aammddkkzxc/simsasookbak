3fdfac8f98923387f4250e5e2acfe78ce19df349
aammddkkzxc <ideal0427@naver.com>
2024-07-21 18:52:31 +0900

    refactor: 중복 호출 방지 로직 time -> flag로 변경

--- src/main/java/com/simsasookbak/global/aop/MethodExecutionRestrictionAspect.java
+++ src/main/java/com/simsasookbak/global/aop/MethodExecutionRestrictionAspect.java
@@ -5,6 +5,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -15,25 +19,39 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Aspect
 @Component
 public class MethodExecutionRestrictionAspect {
-    private final Map<String, Long> lastInvocationTimes = new HashMap<>();
+
+    private final Map<String, Boolean> methodExecutionFlags = new ConcurrentHashMap<>();
 
     @Pointcut("@annotation(com.simsasookbak.global.aop.MethodInvocationLimit)")
     private void methodInvocationLimit() {}
 
     @Before("methodInvocationLimit()")
-    public void beforeMethodInvocationLimit() {
+    public void beforeMethodInvocationLimit(JoinPoint joinPoint) {
         HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(
                 RequestContextHolder.getRequestAttributes())).getRequest();
         String username = request.getUserPrincipal().getName();
+        String methodName = joinPoint.getSignature().getName();
+        String key = username + ":" + methodName;
 
-        Long lastInvocationTime = lastInvocationTimes.get(username);
-        long currentTime = System.currentTimeMillis();
+        Boolean isMethodRunning = methodExecutionFlags.get(key);
 
-        if (lastInvocationTime != null && currentTime - lastInvocationTime < 25 * 1000) {
-            throw new MethodInvocationLimitException("같은 사용자가 반복적인 호출을 시도하였습니다");
+        if (Boolean.TRUE.equals(isMethodRunning)) {
+            throw new MethodInvocationLimitException("같은 사용자가 아직 처리중인 메서드를 반복 호출하였습니다");
         }
 
-        // 메서드를 호출한 시간을 기록
-        lastInvocationTimes.put(username, currentTime);
+        // 메서드 실행 상태 플래그 설정
+        methodExecutionFlags.put(key, true);
+    }
+
+    @After("methodInvocationLimit()")
+    public void afterMethodInvocationLimit(JoinPoint joinPoint) {
+        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(
+                RequestContextHolder.getRequestAttributes())).getRequest();
+        String username = request.getUserPrincipal().getName();
+        String methodName = joinPoint.getSignature().getName();
+        String key = username + ":" + methodName;
+
+        // 메서드 실행 상태 플래그 해제
+        methodExecutionFlags.put(key, false);
     }
 }
